755f8c7824d73dff6d05d16b61fc3f71320e680c
NickArB <98503047+NickArB@users.noreply.github.com>
2023-02-22 14:07:17 -0500

    Update Registry.java

--- Lab3/ClaseEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ Lab3/ClaseEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -1,11 +1,17 @@
 package edu.eci.cvds.tdd.registry;
+import java.util.ArrayList;
+
 public class Registry {
+	private ArrayList<Person> persons = new ArrayList<Person>();
+	private RegisterResult ans = new RegisterResult();
+	
 	public RegisterResult registerVoter(Person p){
+		ans = (p.getAge() >= 18)? ans.UNDERAGE : (p.isAlive() == true);
 		// TODO Validate person and return real result.
 		return RegisterResult.VALID;
 	}
 }
-
+DEAD, UNDERAGE, INVALID_AGE, VALID, DUPLICATED
 
 
 
