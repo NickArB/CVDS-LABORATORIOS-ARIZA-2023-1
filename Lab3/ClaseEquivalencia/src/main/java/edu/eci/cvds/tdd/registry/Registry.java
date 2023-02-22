5bc3f821eea2285ed3e342a5ca6bad8d598575bc
NickArB <98503047+NickArB@users.noreply.github.com>
2023-02-22 16:53:44 -0500

    Update Registry.java

--- Lab3/ClaseEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ Lab3/ClaseEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -5,14 +5,25 @@ public class Registry {
 	private ArrayList<Person> persons = new ArrayList<Person>();
 	private RegisterResult ans = new RegisterResult();
 	
-	public RegisterResult registerVoter(Person p){
-		ans = (p.getAge() >= 18)? ans.UNDERAGE : (p.isAlive() == true);
-		// TODO Validate person and return real result.
-		return RegisterResult.VALID;
+	public static RegisterResult registerVoter(Person p){
+		
+		ans = validateAge(p);
+		return (ans != RegisterResult.VALID) ? ans : (p.isAlive()) ? RegisterResult.DEAD : (validateIsDuplicated(p)) ? RegisterResult.DUPLICATED :  RegisterResult.VALID;
+	}
+	
+	private static RegisterResult validateAge(Person p){
+		int age = p.getAge();
+		return (age < 18 && age >= 0) ? RegisterResult.UNDERAGE : (age > 160 || age < 0) ? RegisterResult.INVALID_AGE : RegisterResult.VALID;
+	}
+	
+	private static boolean validateIsDuplicated(Person p){
+		boolean duplicated = false;
+		for (int i = 0; i < persons.size(); i++){
+			if (persons.get(i).getId() == p.getId()){
+				duplicated = true;
+				break;
+			}
+		}
+		return duplicated;
 	}
 }
-DEAD, UNDERAGE, INVALID_AGE, VALID, DUPLICATED
-
-
-
-
