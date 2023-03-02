a9d2d85f124290b547661d544a22552677c88933
Nicolas <nicolas.ariza241@gmail.com>
2023-03-02 18:41:13 -0500

    Actualizacion pom y diseño de metodos

--- Lab4/src/main/java/hangman/model/BonusScore.java
+++ Lab4/src/main/java/hangman/model/BonusScore.java
@@ -3,10 +3,21 @@ package hangman.model;
 public class BonusScore implements GameScore{
 	private int points;
 	
+	/**
+	*	Method that calculates the score of the player based in how many
+	*	correct or incorrect letters he guessed.
+	*	@Param correctCount The number of letters that the player have asserted.
+	*	@Param ncorrectCount The number of letters that the player have failed guessing.
+	*	@Return The current score of the player based of his positive or negative points.
+	*/
 	public int calculateScore(int correctCount, int incorrectCount){
 		return 1;
 	}
 	
+	/**
+	*	Method that returns the current score of a player.
+	*	@Return The current score of the player.
+	*/
 	public int getPoints(){
 		return this.points;
 	}
--- Lab4/src/main/java/hangman/model/OriginalScore.java
+++ Lab4/src/main/java/hangman/model/OriginalScore.java
@@ -3,11 +3,21 @@ package hangman.model;
 public class OriginalScore implements GameScore{
 	private int points = 100;
 	
-    public int calculateScore(int correctCount, int incorrectCount){
+	/**
+	*	Method that calculates the score of the player based in how many
+	*	correct or incorrect letters he guessed.
+	*	@Param ncorrectCount The number of letters that the player have failed guessing.
+	*	@Return The current score of the player based of his positive or negative points.
+	*/
+    public int calculateScore(int incorrectCount){
 		int ans = getPoints() - incorrectCount*10;
 		return (getPoints() == 0 | ans <= 0) ? 0 : ans;
 	}
 	
+	/**
+	*	Method that returns the current score of a player.
+	*	@Return The current score of the player.
+	*/
 	public int getPoints(){
 		return this.points;
 	}
