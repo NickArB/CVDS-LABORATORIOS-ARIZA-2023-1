6ee9360c8b8b8e6706ecba0821ea4733285bd358
unknown <nicolas.ariza@labinfo.is.escuelaing.edu.co>
2023-03-02 14:23:28 -0500

    Avance metodos

--- Lab4/src/main/java/hangman/model/OriginalScore.java
+++ Lab4/src/main/java/hangman/model/OriginalScore.java
@@ -1,10 +1,11 @@
 package hangman.model;
 
 public class OriginalScore implements GameScore{
-	private int points;
+	private int points = 100;
 	
     public int calculateScore(int correctCount, int incorrectCount){
-		return 1;
+		int ans = getPoints() - incorrectCount*10;
+		return (getPoints() == 0 | ans <= 0) ? 0 : ans;
 	}
 	
 	public int getPoints(){
