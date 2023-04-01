3a3b67415558fb14f2003a1239ae3ba69ebcb9eb
NickArB <98503047+NickArB@users.noreply.github.com>
2023-04-01 08:53:29 -0500

    Create Employee.java

---
+++ Lab7/src/main/java/com/app/model/Employee.java
@@ -0,0 +1,13 @@
+@Entity
+public class Employee {
+	
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long employeeId;
+  private String firstName;
+  private String lastName;
+  private String role;
+  private Double salary;
+  
+  
+}
